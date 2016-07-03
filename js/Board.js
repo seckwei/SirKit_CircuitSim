2d8d29ec32fd3ebb8530412e613e0d2c0c86bd08
Seck Wei <seckwei93@gmail.com>
2016-07-04 00:18:00 +0100

    Updating Board module to JSDoc comments

--- js/Board.js
+++ js/Board.js
@@ -1,3 +1,8 @@
+/**
+ * Board module
+ * @module Board
+ */
+
 'use strict';
 
 /*
@@ -8,6 +13,15 @@
     arrays e.g., [[0,0],[4,1]]
 */
 
+
+/**
+ * Board
+ * 
+ * @param {number} [width=10] Width of the board - 0 indexed
+ * @param {number} [height=10] Height of the board - 0 indexed
+ * @returns {Board}
+ * @instance
+ */
 let Board = function Board(width = 10, height = 10) {	
 	// Initialise empty board
 	let	board = Array.apply(null, { length: width });
@@ -21,7 +35,12 @@ let Board = function Board(width = 10, height = 10) {
     // Closed Circuit flag
     let closed = false;
     
-    // Private - Initialise Slot if position is undefined
+    /**
+     * Initialise a Slot if it hasn't already been done.
+     * 
+     * @param {Array} pos e.g. [1,1]
+     * @private
+     */
     function initSlot(pos){
         let [x,y] = pos;
         if(!board[x][y]){
@@ -29,13 +48,24 @@ let Board = function Board(width = 10, height = 10) {
         }
     }
     
-    // Public - Return the slot specified by the position passed in
+    /**
+     * Return the slot specified by the position given
+     * 
+     * @param {Array} pos
+     * @returns {Slot}
+     * @private
+     */
     function getSlot(pos){
         return board[pos[0]][pos[1]];
     }
 	
-	// Place component pins into respective slots
-    // positions is a 2D array e.g. [[0,1], [1,1]]
+	/**
+	 * Place Component pins into respective Slots
+	 * 
+	 * @param {Component} component
+	 * @param {Array} positions
+     * @see Use [Component's place]{@link Component#place} method
+	 */
 	function place(component, positions) {
         
 		positions.forEach((position, index) => {			
@@ -52,7 +82,12 @@ let Board = function Board(width = 10, height = 10) {
         addComponent(component);
 	}
 	
-    // Remove component pins from their slot
+	/**
+	 * Remove Component from its Slots
+	 * 
+	 * @param {Component} component
+     * @see Use [Component's remove]{@link Component#remove} method
+	 */
 	function remove(component){
 		component.pins.forEach((position) => {
 			let x = position[0],
@@ -68,35 +103,59 @@ let Board = function Board(width = 10, height = 10) {
         removeComponent(component);
 	}
     
-    // Private - Add to list of occupied slots, if havent already
+    /**
+     * Adds a position to a list of occupied Slots
+     * 
+     * @param {Array} pos
+     * @private
+     */
     function addOccupiedSlot(pos){
         if(!occupiedSlots.has(pos.toString())){
             occupiedSlots.set(pos.toString(), board[pos[0]][pos[1]]);
         }
     }
-    
-    // Private - Remove from the list of occupied slots
+
+    /**
+     * Remove position from the list of occupied Slots
+     * 
+     * @param {Array} pos
+     * @private
+     */
     function removeOccupiedSlot(pos){
         if(occupiedSlots.has(pos.toString())){
             occupiedSlots.delete(pos.toString());
         }
     }
 	
-    // Private - Add to list of components on the board
+    /**
+     * Add Component to list of components on the Board
+     * 
+     * @param {Component} component
+     * @private
+     */
     function addComponent(component){
         if(!components.has(component.id)){
             components.set(component.id, component);
         }
     }
     
-    // Private - Remove from list of components on the board
+    /**
+     * Remove Component from list of components on the Board
+     * 
+     * @param {Component} component
+     * @private
+     */
     function removeComponent(component){
         if(components.has(component.id)){
             components.delete(component.id, component);
         }
     }
     
-    // Public - Get array of active sources
+    /**
+     * Get array of active source Components
+     * 
+     * @returns {Array}
+     */
     function getActiveSources(){
         let actSources = [];
         for(let component of components.values()){
@@ -107,7 +166,11 @@ let Board = function Board(width = 10, height = 10) {
         return actSources;
     }
     
-    // Public - returns true if any true node has any untraveled components
+    /**
+     * Returns true if any true node has any untraveled components
+     * 
+     * @returns {Boolean}
+     */
     function hasUnfinishedNode(){
         let hasUnfinished = false;
         for(let slot of occupiedSlots.values()){
@@ -119,7 +182,12 @@ let Board = function Board(width = 10, height = 10) {
         return hasUnfinished;
     }
     
-    // Public - returns true if the passed in component type is found on the board
+    /**
+     * Returns true if Component Type is found on the Board
+     * 
+     * @param {ComponentType} type
+     * @returns {Boolean}
+     */
     function hasType(type){
         let found = false;
         for(let component of components.values()){
