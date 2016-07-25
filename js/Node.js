8723cf9db70f6791ebdc21fb498288235181d86e
Seck Wei <seckwei93@gmail.com>
2016-07-25 22:02:09 +0100

    Added an id to the Node so that we have a some sort of reference to it when producing the KCL equations.

--- js/Node.js
+++ js/Node.js
@@ -11,6 +11,7 @@ class Node {
         if(!position || !(position instanceof Array))
             Utility.logger('Node needs to be initiliased with a position passed in');
 
+        this.id = Node.getID();
         this.position = position;
         this.branches = [];
         this.KCL = undefined;
@@ -20,3 +21,13 @@ class Node {
         this.branches.push(branch);
     }
 }
+
+Node.counter = 0;
+
+Node.resetCounter = function() {
+    Node.counter = 0;
+};
+
+Node.getID = function() {
+    return 'N' + (Node.counter++);
+};
